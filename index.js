a073296fabab22299fdfc6a4e277d713ed82a6a7
Christopher Anderson <chrande@outlook.com>
2015-05-31 23:36:23 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Chris's Samples</title>
+</head>
+<body>
+<div>
+<h1>Samples</h1>
+<p>
+  <ul>
+    <li>
+      <a href="/numbers">Numbers</a>
+    </li>
+  </ul>
+</p>
+</div>
+</body>
+</html>
